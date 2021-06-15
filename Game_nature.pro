e07eea98ace7364d6d74efa6a96b6cb7f2a29815
Alex Krieg <alexkrieg@gmx.ch>
2021-06-15 22:22:54 +0200

    init

--- Game_nature.pro
+++ Game_nature.pro
@@ -7,7 +7,7 @@ CONFIG -= app_bundle
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-ENGINE_PATH = ../PixelEngine/PixelEngine
+ENGINE_PATH = $$PWD/extern/PixelEngine/PixelEngine
 include($$ENGINE_PATH/PixelEngine.pri)
 
 SOURCES += \
--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <QCoreApplication>
+#include "pixelengine.h"
+
+PixelEngine     *engine;
+
+void setup_level();
+void clear_level();
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    setup_level();
 
-    return a.exec();
+    while(engine->running())
+    {
+        engine->checkEvent();
+        engine->tick();
+        engine->display();
+        if(engine->get_setting_displayInterval()  > 0.002 &&
+           engine->get_setting_gameTickInterval() > 0.002 &&
+           engine->get_setting_eventHandleInterval() > 0.002)
+        {
+            nanosleep((const struct timespec[]){{0, 500000L}}, NULL);
+        }
+    }
+    clear_level();
+    return 1;
+}
+void setup_level()
+{
+    unsigned int mapWidth = 300;
+
+    RECT dispaySize;
+    GetWindowRect(GetDesktopWindow(), &dispaySize);
+    PointU windowSize(dispaySize.right,dispaySize.bottom-20);
+    engine = new PixelEngine (PointU(mapWidth,double(mapWidth)*double(windowSize.getY())/double(windowSize.getX())),windowSize);
+    engine->set_setting_checkEventInterval(1.0f/30.0f);
+    engine->set_setting_gameTickInterval(1.0f/120.0f);
+    engine->set_setting_displayInterval(1.0f/60.0f);
+
+    engine->display_stats(true);
+}
+void clear_level()
+{
+
+    delete engine;
 }
